e9741ed2fa8c7dfdf95fcc1d91720839e7bb093f
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2021-06-09 19:30:28 +0300

    init

---
+++ js/arrays.js
@@ -0,0 +1,151 @@
+/* дані для анімації кружечків з графіків*/
+const array_1 = [
+    {id: "circle_1_1",
+        cx_to:843.2, cy_to:525, r_to: 84.7,
+        cx_from:733.1, cy_from:347.9, r_from:268.1, opacity: 0.35
+    },
+    {id: "circle_1_2",
+        cx_to:672.2, cy_to:533.3, r_to:23.9,
+        cx_from:398.7, cy_from:243.4, r_from:75.8, opacity: 0.35
+    },
+    {id: "circle_1_3",
+        cx_to:682.9, cy_to:596.6, r_to:10.7,
+        cx_from:416.5, cy_from:357.6, r_from:33.9, opacity: 0.65
+    },
+    {id: "circle_1_4",
+        cx_to:612.2, cy_to:541.3, r_to:6.3,
+        cx_from:436.6, cy_from:440.3, r_from:20.1, opacity:0.72
+    },
+    {id: "circle_1_5",
+        cx_to:731.8, cy_to:557.3, r_to:10.2,
+        cx_from:461.8, cy_from:507.3, r_from:32.2, opacity:0.72
+    },
+    {id: "circle_1_6",
+        cx_to:620.8, cy_to:573.5, r_to:6.1,
+        cx_from:473.7, cy_from:167.1, r_from:10.7, opacity:0.72
+    }
+];
+
+
+const array_2 = [
+    {id: "circle_2_1",
+        cx_to:412, cy_to:441, r_to:180.8,
+        cx_from:826.7, cy_from:188.5, r_from:186.75, opacity:0.72
+    },
+    {id: "circle_2_2",
+        cx_to:605.8, cy_to:164, r_to:153.4,
+        cx_from:612.8, cy_from:461, r_from:158.4, opacity:0.72
+    },
+    {id: "circle_2_3",
+        cx_to:153, cy_to:156, r_to:141.7,
+        cx_from:301.4, cy_from:409.8, r_from:146.4, opacity:0.65
+    },
+    {id: "circle_2_4",
+        cx_to:118, cy_to:505, r_to:117.3,
+        cx_from:506.8, cy_from:198.29236, r_from:121.2, opacity:0.65
+    },
+    {id: "circle_2_5",
+        cx_to:696, cy_to:402, r_to:92.8,
+        cx_from:871.3, cy_from:468.9, r_from:95.8, opacity:0.72
+    },
+    {id: "circle_2_6",
+        cx_to:378, cy_to:132, r_to:33.9,
+        cx_from:408.8, cy_from:559.3, r_from:35, opacity:0.72
+    },
+    {id: "circle_2_7",
+        cx_to:324.8, cy_to:189.7, r_to:11.2,
+        cx_from:294.4, cy_from:126.4, r_from:11.6, opacity:0.72
+    },
+    {id: "circle_2_8",
+        cx_to:366, cy_to:210.6, r_to:8.3,
+        cx_from:295.8, cy_from:246, r_from:10.2, opacity:0.72
+    },
+    {id: "circle_2_9",
+        cx_to:324.8, cy_to:230.8, r_to:7.3,
+        cx_from:294.4, cy_from:166.4, r_from:7, opacity:0.72
+    },
+
+    {id: "circle_2_10",
+        cx_to:412.4, cy_to:210.6, r_to:6.8,
+        cx_from:294.4, cy_from:206.6, r_from:7.6
+    }
+
+];
+
+const array_4 = [
+    {id: "circle_4_1",
+        cx_to:786.8, cy_to:60.4, r_to:34.7,
+        cx_from:200, cy_from:423.9, r_from:189.1, opacity:0.72
+    },
+    {id: "circle_4_2",
+        cx_to:916, cy_to:79, r_to:29.8,
+        cx_from:550.2, cy_from:457.8, r_from: 159.75, opacity:0.72
+    },
+    {id: "circle_4_3",
+        cx_to:857.2, cy_to:106.5, r_to:24.4,
+        cx_from:422.8, cy_from:190.3, r_from:130.5, opacity:0.72
+    },
+    {id: "circle_4_4",
+        cx_to:862, cy_to:60, r_to:13.1,
+        cx_from:612.7, cy_from:131.8, r_from:63.9, opacity:0.72
+    },
+    {id: "circle_4_5",
+        cx_to:813, cy_to:109, r_to:10.7,
+        cx_from:653.7, cy_from:254.1, r_from:58.3, opacity:0.72
+    },
+    {id: "circle_4_6",
+        cx_to:843, cy_to:27.3, r_to:10.2,
+        cx_from:770.3, cy_from:358, r_from:70.6, opacity:0.72
+    },
+    {id: "circle_4_7",
+        cx_to:862.5, cy_to:60, r_to:13.1,
+        cx_from:776.2, cy_from:511, r_from:53.6, opacity:0.72
+    },
+    {id: "circle_4_8",
+        cx_to:836, cy_to:66, r_to:5.4,
+        cx_from:845.6, cy_from:440.6, r_from:28.8, opacity:0.72
+    },
+    {id: "circle_4_9",
+        cx_to:915.9, cy_to:30, r_to:10,
+        cx_from:422.8, cy_from:231.6, r_from:52.2, opacity:0.72
+    }
+
+];
+
+const array_5 = [
+    {id: "circle_5_1",
+        cx_to:821.5, cy_to:219.5, r_to:63.4,
+        cx_from:773.2, cy_from:219.3, r_from:203, opacity:0.72
+    },
+    {id: "circle_5_2",
+        cx_to:980.1, cy_to:43.9, r_to:29.3,
+        cx_from:907.9, cy_from:510.4, r_from:111.2, opacity:0.72
+    },
+    {id: "circle_5_3",
+        cx_to:950.8, cy_to:167.1, r_to:52.6,
+        cx_from:474.2, cy_from:452.9, r_from:168.7, opacity:0.72
+    },
+    {id: "circle_5_4",
+        cx_to:980.1, cy_to:90, r_to:13.8,
+        cx_from:473.8, cy_from:166.06, r_from:43.8, opacity:0.72
+    },
+    {id: "circle_5_5",
+        cx_to:1015.1, cy_to:120.9, r_to:7.2,
+        cx_from:413.8, cy_from:236.1, r_from:22.6, opacity:0.72
+    }
+];
+
+const array_6 = [
+    {id: "circle_6_1",
+        cx_to:951.0, cy_to:316.8, r_to:77.5,
+        cx_from:293.70001, cy_from:359.2, r_from:228, opacity:0.72
+    },
+    {id: "circle_6_2",
+        cx_to:980.2, cy_to:460.26, r_to:59.2,
+        cx_from:609.90002, cy_from:198.1, r_from:119.1, opacity:0.72
+    },
+    {id: "circle_6_3",
+        cx_to:836, cy_to:377.3, r_to:39.6,
+        cx_from:796.02625, cy_from:439.14026, r_from:177.99, opacity:0.72
+    }
+];
