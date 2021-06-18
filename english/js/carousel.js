3b1d33bb17fb66fd51d3cb2ad741458265e420bd
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2021-06-18 13:42:14 +0300

    англ. версія

---
+++ english/js/carousel.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yevheniia on 18.06.21.
+ */
+d3.selectAll(".preview img").on("click", function(){
+    var clicked_src = d3.select(this).attr("src");
+
+    d3.selectAll(".preview img").classed("gallery-hidden-pic", false);
+    d3.select(this).classed("gallery-hidden-pic", true);
+
+    d3.select(".gallery-main-pic img").attr("src", clicked_src);
+
+});
